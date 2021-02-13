fc61f9bca161573705a135156c915f408414a12a
Treyor <Smenyailo@bk.ru>
2021-02-13 14:28:16 +0300

    Add files via upload

---
+++ assets/js/settingUp.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+
+        // const slides = document.querySelectorAll('.slides li')
+        // const flexControl = document.querySelectorAll('.flex-control-thumbs img')
+        const flexControl = document.querySelector('.flex-control-thumbs')
+        // const next = document.querySelector('.flex-next')
+        // const prev = document.querySelector('.flex-prev')
+        flexControl.style.display = 'none';
+        // const array_size = 4;
+    
+        // const sliced_array = [];
+    
+        // for (let i = 0; i <flexControl.length; i += array_size) {
+        //     sliced_array.push(Object.values(flexControl).slice(i, i + array_size));
+        // }
+    
+        // console.log(sliced_array);
+    
+        // function checkItem() {
+        //     console.log(11)
+        // }
+    
+        // next.addEventListener('click', checkItem());
+
+}
